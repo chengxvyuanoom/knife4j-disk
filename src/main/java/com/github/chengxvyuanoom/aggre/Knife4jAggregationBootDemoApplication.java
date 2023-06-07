795dca87968e6f30efc257825f6f62e0d9ef888d
chengxvyuanoom <490466922@qq.com>
2023-06-07 21:46:24 +0800

    first commit

---
+++ src/main/java/com/github/chengxvyuanoom/aggre/Knife4jAggregationBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.chengxvyuanoom.aggre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knife4jAggregationBootDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Knife4jAggregationBootDemoApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/chengxvyuanoom/aggre/Knife4jAggregationBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chengxvyuanoom.aggre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knife4jAggregationBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
